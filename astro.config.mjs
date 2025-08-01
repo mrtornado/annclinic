5b2d040f175c073732010c39536b8e337b035582
mrtornado <mrtornado@gmail.com>
2025-08-01 15:47:35 +0300

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
-import { defineConfig, fontProviders } from "astro/config";
+import {
+  defineConfig,
+  fontProviders,
+  passthroughImageService,
+} from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
@@ -39,6 +43,11 @@ export default defineConfig({
   build: {
     inlineStylesheets: "auto",
   },
+  image: {
+    service: passthroughImageService(),
+    responsiveStyles: true,
+  },
+
   vite: {
     // CSS optimization
     css: {
